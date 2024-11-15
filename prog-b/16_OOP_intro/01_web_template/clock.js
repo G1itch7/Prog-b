45bfae498a4831a008843ca072452f5b565c0147
G1itch7 <mariosonic@hotmail.dk>
2024-11-15 11:20:55 +0100

    Clock :moyai:

--- prog-b/16_OOP_intro/01_web_template/clock.js
+++ prog-b/16_OOP_intro/01_web_template/clock.js
@@ -2,8 +2,10 @@ console.log("clock is here")
 
 class Clock{
     //constructor er klassens "setup" funktion, som kaldes når vi opretter nye objekter med klassen
-    constructor(div){
+    //kaldes polymorfi
+    constructor(div, style){
         this.div = div
+        this.style = style
         //divs for hours, minutes and seconds 
         this.hDiv = createDiv()
         this.mDiv = createDiv()
@@ -13,6 +15,22 @@ class Clock{
         this.div.child(this.sDiv)
         //interval til at sætte tiden ind.
         this.interval
+        //style
+        this.div.style('width','16rem')
+        this.div.style('height','5rem')
+        this.div.style('border','4px dotted gray')
+        this.div.style('display','grid')
+        this.div.style('place-items','center')
+        this.div.style('padding','1rem')
+        this.div.style('border-radius','2rem')
+        switch(style){
+            case "pink": this.div.style('background','hotpink')
+                return
+            case "black": 
+                this.div.style('background','black')
+                this.div.style('color','white')
+                return
+        }
     }
     start(){
         this.interval = setInterval(()=>{
@@ -20,5 +38,9 @@ class Clock{
             this.mDiv.html(minute() < 10 ? '0' + minute() : minute())
             this.sDiv.html(second() < 10 ? '0' + second() : second())
         },1000)
+    }
+    stop(){
+        //holy fucking shit the thing stopped
+        clearInterval(this.interval)
     }
 }
--- prog-b/16_OOP_intro/01_web_template/index.js
+++ prog-b/16_OOP_intro/01_web_template/index.js
@@ -1,5 +1,5 @@
 function setup(){
     //variablen c, er en instans af klassen clock, som får div'en #clock med i sin construct
-    let c = new Clock(select('#clock'))
+    let c = new Clock(select('#clock'),'pink')
     c.start()
 }
