bcbdba9f898e918f1bae83c8abde1c05f51a120a
G1itch7 <mariosonic@hotmail.dk>
2024-12-27 05:41:14 +0100

    making OOP stuff work

---
+++ prog-b/18_more_OOP_thing/advice.js
@@ -0,0 +1,34 @@
+console.log('cheese')
+
+class Advice {
+    constructor(apiURL,containerDiv){
+        this.containerDiv = containerDiv
+        this.apiURL = apiURL
+        this.fetchData()
+    }
+
+    //fetcher data
+    fetchData(){
+        fetch('https://api.adviceslip.com/advice')
+        .then(
+            function(response){
+                console.log(response)
+                return response.json()
+            }
+        )
+        .then(
+            function(data){
+                console.log(data.slip.advice)
+                //laver div til advice data
+                    let slipDiv = createDiv('Slip: '+ data.slip.id)
+                    let adviceDiv = createDiv(data.slip.advice)
+                    
+            }
+        )
+        //laver en div til at separere hvert advice
+        this.newDiv = createDiv()
+        this.newDiv.child(this.slipDiv)
+        this.newDiv.child(this.adviceDiv)
+        this.containerDiv.child(this.newDiv)
+    }
+}
---
+++ prog-b/18_more_OOP_thing/index.js
@@ -0,0 +1,7 @@
+console.log('cheese 2')
+
+
+function setup(){
+    new Advice('https://api.adviceslip.com/advice',select('#container'))
+    new Advice('https://api.adviceslip.com/advice',select('#container'))
+}
