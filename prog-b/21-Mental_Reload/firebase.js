9052877c106b25d7499ce473bd342b92dabb4d7f
G1itch7 <mariosonic@hotmail.dk>
2025-04-01 08:52:37 +0200

    Starting mental reload

---
+++ prog-b/21-Mental_Reload/firebase.js
@@ -0,0 +1,23 @@
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCqnXBtQymHZo5B8-1IzF4HgT00C0_cWxk",
+  
+    authDomain: "mental-reload-95d66.firebaseapp.com",
+  
+    projectId: "mental-reload-95d66",
+  
+    storageBucket: "mental-reload-95d66.firebasestorage.app",
+  
+    messagingSenderId: "634955541388",
+  
+    appId: "1:634955541388:web:64a9e7fc037ead46f1c4a9"
+  
+  };
+  
+  
+  // Initialize Firebase
+  
+  const app = initializeApp(firebaseConfig);
+  const database = firebase.firestore()
