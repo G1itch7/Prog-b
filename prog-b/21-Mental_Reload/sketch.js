a58150158d5025f11d83f70bcb6e8f6ec561bcb1
G1itch7 <mariosonic@hotmail.dk>
2025-04-08 09:47:47 +0200

    Yippie it works

--- prog-b/21-Mental_Reload/sketch.js
+++ prog-b/21-Mental_Reload/sketch.js
@@ -1,14 +1,41 @@
 //VIEW MODEL CONTROLLER 
 let dataModel
-let todosDiv
 
 function setup() {
   noCanvas()
-  database.collection('Presets').doc('Preset')
+  database.collection('reload').doc('presets')
     .onSnapshot( (doc) => {
     console.log('Fik dette fra databasen: ', doc.data() )
     dataModel = doc.data()
+    
     })
+    
+    
 }
 
+function mousePressed(){
+  console.log('mouse pressed')
+  doDaThing()
+}
 
+let name = "chees"
+let r =22
+let g = 23
+let b = 24
+let speed = 0.1
+let rotate = "fast" 
+
+function doDaThing(name, r, g, b, speed, rotate){
+  let cheese = {
+    "x": 2,
+    "y": 1,
+  }
+  dataModel.preset.push(cheese)
+  database.collection('reload').doc('presets').set(dataModel)
+  .then( ()=>{
+    console.log('database updated', cheese)
+  })
+  .catch( (error) => {
+    console.log('det gik mindre godt', error)
+  })
+}
